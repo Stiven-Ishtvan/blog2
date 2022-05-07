1f7090efee202df0f7db7965ec0fccbbe61842a8
Stepan <Stiven-Ishtvan>
2022-05-07 20:42:26 +0300

    Блог2

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { PostList } from "./components/PostList";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { AddPost } from "./components/AddPost";
+import api from "./utils/api";
+import { Routes, Route, Link } from "react-router-dom";
+import { PostPage } from "./components/PostPage";
+
+function App() {
+  const [posts, setPosts] = useState([]);
+  const [userInfo, setUserInfo] = useState([]);
+  const [like, setLike] = useState(JSON.parse(localStorage.getItem("likes")));
+
+  useEffect(() => {
+    api.getPosts().then((result) => {
+      setPosts(result.reverse());
+    });
+  }, []);
+
+  useEffect(() => {
+    api.getUserInfo().then((result) => {
+      setUserInfo(result);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header userInfo={userInfo} />
+      
+      <Routes>
+        <Route
+          path="/"
+          element={<div>
+            
+            <PostList
+              mapPosts={posts}
+              like={like}
+              setLike={setLike}
+              userInfo={userInfo}
+            />
+            </div>
+          }
+        />
+        <Route path="create" element={<AddPost />} />
+        <Route path="posts/:postID" element={<PostPage />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
